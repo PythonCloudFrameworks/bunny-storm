a8da2cc9ab90e4da941947e833df33d6007b8565
Roee <roygolan123@gmail.com>
2020-04-19 19:57:46 +0300

    Improved logger, added reconnect, and modified future handling

--- tornado_bunny/async_connection.py
+++ tornado_bunny/async_connection.py
@@ -1,11 +1,10 @@
-import datetime
-import functools
+import sys
 import logging
 
+from cached_property import cached_property
 from pika import URLParameters, ConnectionParameters
 from pika.adapters.tornado_connection import TornadoConnection
 from tornado import gen
-from tornado.concurrent import Future
 from tornado.queues import Queue
 
 
@@ -17,17 +16,23 @@ class AsyncConnection(object):
     TIMEOUT_STATUS = 'timeout'
 
     def __init__(self, rabbitmq_url, io_loop, timeout=10):
-        self._parameter = ConnectionParameters("127.0.0.1") if rabbitmq_url in ["localhost", "127.0.0.1"] else \
+        self.should_reconnect = False
+
+        self._parameters = ConnectionParameters("127.0.0.1") if rabbitmq_url in ["localhost", "127.0.0.1"] else \
             URLParameters(rabbitmq_url)
         self._io_loop = io_loop
         self._timeout = timeout
-        self._logger = logging.getLogger(__name__)
-        self._queue = Queue(maxsize=1)
+        self._connection_queue = Queue(maxsize=1)
         self._current_status = self.INIT_STATUS
 
-    @property
+    @cached_property
     def logger(self):
-        return self._logger
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.DEBUG)
+        sh = logging.StreamHandler(sys.stdout)
+        sh.setLevel(logging.DEBUG)
+        logger.addHandler(sh)
+        return logger
 
     @property
     def status_ok(self):
@@ -38,55 +43,65 @@ class AsyncConnection(object):
     def get_connection(self):
         if self._current_status == self.INIT_STATUS:
             self._current_status = self.CONNECTING_STATUS
-            yield self._connect()
+            self._connect()
         conn = yield self._top()
-        raise gen.Return(conn)
+        return conn
 
-    @gen.coroutine
     def _connect(self):
         try:
-            connection = yield self._try_connect()
-            if connection is not None:
-                self._queue.put(connection)
+            self._try_connect()
         except Exception as e:
             self.logger.error("failed to connect rabbitmq. %s", e)
 
     @gen.coroutine
     def _top(self):
-        conn = yield self._queue.get()
-        self._queue.put(conn)
-        raise gen.Return(conn)
+        conn = yield self._connection_queue.get()
+        self._connection_queue.put(conn)
+        return conn
 
-    def _on_timeout(self, future):
+    def _on_timeout(self):
         if self._current_status == self.CONNECTING_STATUS:
-            self.logger.error("creating connection time out")
+            self.logger.error("creating connection timed out")
             self._current_status = self.TIMEOUT_STATUS
-            future.set_exception(Exception("connection Timeout"))
+            self.stop()
+
+    def _open_callback(self, connection):
+        self.logger.info("created connection")
+        self._current_status = self.OPEN_STATUS
+        self._connection_queue.put(connection)
+
+    def _open_error_callback(self, connection, exception):
+        self.logger.error(f"open connection with error: {exception}")
+        self._current_status = self.CLOSE_STATUS
+        self.reconnect()
+
+    def _close_callback(self, connection, reply_code, reply_text):
+        self.logger.error(f"closing connection: reply code: {reply_code}, reply_text: {reply_text}. system will exist")
+        self._current_status = self.CLOSE_STATUS
+        self.reconnect()
+
+    def reconnect(self):
+        self.should_reconnect = True
+        self.stop()
+
+    def stop(self):
+        self.logger.info('Stopping')
+        try:
+            self._io_loop.stop()
+        except:
+            pass
+
+        if self.should_reconnect:
+            self._current_status = self.INIT_STATUS
+            self._connect()
+            self._io_loop.start()
 
     def _try_connect(self):
         self.logger.info("creating connection")
-        future = Future()
-        self._io_loop.add_timeout(datetime.timedelta(seconds=self._timeout),
-                                  functools.partial(self._on_timeout, future=future))
+        self._io_loop.call_later(self._timeout, self._on_timeout)
 
-        def open_callback(unused_connection):
-            self.logger.info("created connection")
-            self._current_status = self.OPEN_STATUS
-            future.set_result(unused_connection)
-
-        def open_error_callback(connection, exception):
-            self.logger.error("open connection with error: %s", exception)
-            self._current_status = self.CLOSE_STATUS
-            future.set_exception(exception)
-
-        def close_callback(connection, reply_code, reply_text):
-            self.logger.error("closing connection: reply code:%s, reply_text: %s. system will exist",
-                              reply_code, reply_text)
-            self._current_status = self.CLOSE_STATUS
-
-        TornadoConnection(self._parameter,
-                          on_open_callback=open_callback,
-                          on_open_error_callback=open_error_callback,
-                          on_close_callback=close_callback,
+        TornadoConnection(self._parameters,
+                          on_open_callback=self._open_callback,
+                          on_open_error_callback=self._open_error_callback,
+                          on_close_callback=self._close_callback,
                           custom_ioloop=self._io_loop)
-        return future
