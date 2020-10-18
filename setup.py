d9f8a2d3d5e00797cb584d96c25d86e793eceea8
Roee <roygolan123@gmail.com>
2020-10-18 23:03:27 +0300

    Update setup readme type

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
     install_requires=requirements,
     license="MIT license",
     long_description=readme + '\n\n' + history,
+    long_description_content_type='text/markdown',
     include_package_data=True,
     keywords='tornado_bunny',
     name='tornado_bunny',
